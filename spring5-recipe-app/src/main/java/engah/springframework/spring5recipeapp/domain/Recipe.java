9e5a4b27357a14e79b7bf8f4950d2365a5222f46
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-03 18:24:48 +0200

    done all relationships between all entities.

--- spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/Recipe.java
@@ -28,8 +28,15 @@ public class Recipe {
     private String directions;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns =@JoinColumn (name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
     @OneToMany(cascade=CascadeType.ALL,mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -103,5 +110,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
