f69a694516b96af9d23164c5d54fdebb09f03631
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-06 19:37:03 +0200

    done adding @Data and (project lombok).

--- spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/Ingredient.java
+++ spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/Ingredient.java
@@ -1,8 +1,10 @@
 package engah.springframework.spring5recipeapp.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
+@Data
 @Entity
 public class Ingredient {
     private  String description;
@@ -30,43 +32,7 @@ public class Ingredient {
     @OneToOne(fetch = FetchType.EAGER)
 private UnitOfMeasure uom;
 
-    public UnitOfMeasure getUnitOfMeasure() {
-        return uom;
-    }
 
-    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
-        this.uom = unitOfMeasure;
-    }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescriptions(String descriptions) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
--- spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/UnitOfMeasure.java
+++ spring5-recipe-app/src/main/java/engah/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -1,31 +1,13 @@
 package engah.springframework.spring5recipeapp.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Data
 @Entity
 public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-
 }
